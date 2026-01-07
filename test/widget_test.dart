b58a9d34d281ddc40649e3fa4d84120a55054a8f
Hu-Tao128 <alcantarahuerta128@gmail.com>
2026-01-07 13:36:01 -0800

    Codigo autogenerado para uso de Firebase Auth

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:study_medical/main.dart';
+import 'package:study_medical/app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our app builds (finds a Material App or Login Page)
+    // Since we don't know exactly what's in Login Page, just checking if it pumps without error is a good start.
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
