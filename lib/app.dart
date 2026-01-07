46b327549ef28c92196694715f6524f539c25f41
Hu-Tao128 <alcantarahuerta128@gmail.com>
2026-01-07 13:34:37 -0800

    Integracion de routes con app y main .dart

---
+++ lib/app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'l10n/app_localizations.dart';
+import 'core/router/app_router.dart';
+import 'features/auth/data/auth_service.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    final authService = context.watch<AuthService>();
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Study Medical',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: appRouter(authService),
+    );
+  }
+}
